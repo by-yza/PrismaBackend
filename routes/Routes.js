95f7f27c08402d34c920a4ffc87cfc7fea681821
Yza <129904649+by-yza@users.noreply.github.com>
2024-11-21 22:14:14 -0300

    Update Routes.js

--- routes/Routes.js
+++ routes/Routes.js
@@ -3,6 +3,9 @@ const Controller = require('../controllers/Controller')
 
 const router = express.Router()
 
+router.post('/proprietarios/criar',Controller.criarProprietario)
+router.post('/produtos/criar',Controller.criarProduto)
+
 router.get('/proprietarios/',Controller.listarProprietarios)
 router.get('/produtos/',Controller.listarProdutos)
 router.get('/proprietarios/nome/:nome',Controller.proprietarioPorNome)
@@ -10,8 +13,11 @@ router.get('/produtos/maiorquantidade',Controller.produtoMaiorQuantidade)
 router.get('/produtos/maiorvalor',Controller.produtoMaiorValor)
 router.get('/produtos/maiorvalortotal',Controller.produtoMaiorValorTotal)
 router.get('/proprietarios/maiorquantidade',Controller.proprietarioMaiorQuantidade)
-//router.post('/funcionario',FuncionarioController.criarFuncionarios)
-//router.delete('/funcionario/:id', FuncionarioController.deletarFuncionario)
-//router.put('/funcionario/:id',FuncionarioController.atualizarFuncionario)
+
+router.put('/proprietarios/:id',Controller.atualizarProprietario)
+router.put('/produtos/:id',Controller.atualizarProduto)
+
+router.delete('/proprietarios/id/:id', Controller.deletarProprietario)
+router.delete('/produtos/id/:id', Controller.deletarProduto)
 
 module.exports = router;
